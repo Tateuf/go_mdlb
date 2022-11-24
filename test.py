6bade61ada09a8409d4d3daf5eaecf6bc1ea8eff
Noel Logan 18003 <75576766+Tateuf@users.noreply.github.com>
2022-11-24 12:05:12 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-fichier = open("data.txt", "a")
-for i in range(500):
-    fichier.write("\n   {"+str(random.randint(24, 255))+","+str(random.randint(24, 255))+","+str(random.randint(24, 255))+"}"+",")
-fichier.close()
